202784c651f42b63c6e135efb664662d0357cafd
jjstmei <gabriele.pereira.santos@outlook.com>
2024-09-24 21:20:51 -0300

    primeiro commit

---
+++ resolucao.js
@@ -0,0 +1,33 @@
+//AULA 1 EXERCICIO 1
+alert('Boas vindas ao nosso site!');
+
+//AULA 1 EXERCICIO 2
+let nome = "Lua";
+
+//AULA 1 EXERCICIO 3
+let idade = 25;
+
+//AULA 1 EXERCICIO 4
+let numeroDeVendas = 50;
+
+//AULA 1 EXERCICIO 5
+let saldoDisponivel = 1000;
+
+//AULA 1 EXERCICIO 6
+alert('Erro! Preencha todos os campos');
+
+//AULA 1 EXERCICIO 7
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+
+//AULA 1 EXERCICIO 8
+let nome = prompt('Digite seu nome');
+
+//AULA 1 EXERCICIO 9
+let idade = prompt('Digite o seu nome');
+
+//AULA 1 EXERCICIO 10
+let idade = prompt('Dgite a sua idade');
+(idade >= 18) {
+    alert('Pode tirar a habilitação!');
+}
