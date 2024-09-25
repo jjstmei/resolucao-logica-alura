698feb9eb7fa463efcff0d92cd68610deb6f8b93
Gabby <106984548+jjstmei@users.noreply.github.com>
2024-09-24 23:24:13 -0300

    Update resolucao.js

--- resolucao.js
+++ resolucao.js
@@ -31,3 +31,42 @@ let idade = prompt('Dgite a sua idade');
 (idade >= 18) {
     alert('Pode tirar a habilitação!');
 }
+
+//AULA 2 EXERCICIO 1
+
+diaDaSemana = prompt('Qual é o dia da semana?');
+if (diaDaSemana == 'Sábado') {
+    alert('Bom fim de semana!');
+} else if (diaDaSemana == 'Domingo') {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
+}
+
+// AULA 2 EXERCICIO 2
+
+numero = prompt('Digite um positivo ou negativo');
+if (numero > 0) {
+    alert('Número positivo!');
+} else {
+    alert('Número negativo!');
+}
+
+// AULA 2 EXERCICIO 3
+
+pontuacao = 105;
+if (pontuacao >= 100) {
+    console.log('Parabéns, você venceu!');
+} else {
+    console.log('Tente novamente para ganhar.');
+}
+
+// AULA 2 EXERCICIO 4
+
+let saldoConta = 500; // Exemplo de saldo
+alert(`Seu saldo é de R$${saldoConta}.`);
+
+// AULA 2 EXERCICIO 5
+
+let nome = prompt('Qual o seu nome?');
+alert(`Boas vindas ${nome}`);
